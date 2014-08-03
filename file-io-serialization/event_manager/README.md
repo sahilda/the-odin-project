636979af79b0616d01ae0d8f62e8491a6555044b
Sahil Agarwal <sahil@liftoff.io>
2014-08-03 10:10:05 -0700

    Finished the events manager project!

---
+++ file-io-serialization/event_manager/README.md
@@ -0,0 +1,9 @@
+## Event Manager
+
+This app takes a csv list of attendees, and creates a form letter to send out, with contact for their local political reps, and a request to update their number on file.
+
+Based on:
+[The Odin Project](http://www.theodinproject.com/ruby-programming/file-i-o-and-serialization)
+
+-Sahil
+
--- file-io-serialization/event_manager/lib/event_manager.rb
+++ file-io-serialization/event_manager/lib/event_manager.rb
@@ -1,23 +1,55 @@
+
+require 'erb'
 require 'csv'
 require 'sunlight/congress'
-puts "EventManager Initialized!"
+
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
 def clean_zipcode(zipcode)
 	zipcode.to_s[0..4].rjust(5,"0")
 end
 
-def get_legislator_names(legislators)
-	names = legislators.collect do | legislator |
-	  "#{legislator.first_name} #{legislator.last_name}"
+def valid_number?(number)
+	number = number.gsub(/[()\s-.]/,'')
+	
+	if number.nil? || number.size > 11 || number.size < 9
+		return false
+	else
+		return true if number.size == 10
+		return true if number[0] == 1
+	end
+	false
+end
+
+def get_legislator_names(zipcode)
+	Sunlight::Congress::Legislator.by_zipcode(zipcode)
+end
+
+def save_thank_you_letter(id, form_letter)
+	Dir.mkdir("output") unless Dir.exists?("output")
+	filename = "output/thanks_#{id}.html"
+
+	File.open(filename, 'w') do | file |
+		file.puts form_letter
 	end
 end
 
-Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
-contents = CSV.open("event_attendees.csv",headers: true, header_converters: :symbol)
+puts 'EventManager Initialized.'
+
+contents = CSV.open("event_attendees.csv", headers: true, header_converters: :symbol)
+
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new(template_letter)
+
 contents.each do | line |
+	id = line[0]
 	first_name = line[:first_name]
 	zipcode = clean_zipcode(line[:zipcode])
-	legislators = get_legislator_names(Sunlight::Congress::Legislator.by_zipcode(zipcode))
-	puts "#{first_name} #{zipcode} #{legislators.join(", ")}"
+	phone = line[:homephone] if valid_number?(line[:homephone])
+	legislators = get_legislator_names(zipcode)
+
+	form_letter = erb_template.result(binding)
+	save_thank_you_letter(id, form_letter)
 end
+
